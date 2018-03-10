fb245d418d07781d5257bd371aaa1e05b5e55276
barlaensdoonn <brandon.aleson@gmail.com>
2018-03-09 16:01:23 -0800

    combine RFID read with OSC send

--- main.py
+++ main.py
@@ -3,49 +3,44 @@
 # 3/9/18
 # updated 3/9/18
 
-import RPi.GPIO as GPIO
 import MFRC522
+import RPi.GPIO as GPIO
+from osc_basics import osc_client
+
+
+def hexify(uid):
+    hexs = tuple([hex(u) for u in uid])
+
+    return '{} {} {} {}'.format(*hexs)
 
 
 if __name__ == '__main__':
     MIFAREReader = MFRC522.MFRC522()
+    client = osc_client.OSCClient(host='rfidberry.local')
     reading = True
 
     try:
+        print('scanning for cards...')
+
         while reading:
-            # Scan for cards
+            # scan for cards
             (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
-            # If a card is found
+            # if a card is found
             if status == MIFAREReader.MI_OK:
-                print("Card detected")
+                print('card detected')
 
-            # Get the UID of the card and convert uid to tuple for unpacking
+            # get the UID of the card and convert it to hex
             (status, uid) = MIFAREReader.MFRC522_Anticoll()
-            uid = tuple(uid)
+            hexuid = hexify(uid)
+            print('card UID: {}'.format(hexuid))
 
-            # If we have the UID, continue
+            # make sure no errors popped up in the above exchange
             if status == MIFAREReader.MI_OK:
+                # send UID over OSC
+                client.send(hexuid)
 
-                # Print UID
-                print("Card read UID: {}, {}, {}, {}".format(*uid))
-
-                # This is the default key for authentication
-                key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
-
-                # Select the scanned tag
-                MIFAREReader.MFRC522_SelectTag(uid)
-
-                # Authenticate
-                status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
-
-                # Check if authenticated
-                if status == MIFAREReader.MI_OK:
-                    MIFAREReader.MFRC522_Read(8)
-                    MIFAREReader.MFRC522_StopCrypto1()
-                else:
-                    print("Authentication error")
     except KeyboardInterrupt:
-        print('...user exit received...')
+        print('\n...user exit received...')
         reading = False
         GPIO.cleanup()
