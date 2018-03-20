9097cb3b404c3530259d0eaca17ae10692b5e8a5
barlaensdoonn <brandon.aleson@gmail.com>
2018-03-20 13:52:47 -0700

    move logging related code into its own module

---
+++ log_util.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+# utilities to configure logging
+# 3/9/18
+# updated 3/20/18
+
+'''
+logging configuration utilities for MFRC522
+log filename is based on hostname of the Pi, e.g 'rfidberry01.log'
+'''
+
+
+import os
+import yaml
+import logging
+import logging.config
+from socket import gethostname
+
+
+def _initialize_logger():
+    logger = logging.getLogger('main')
+    logger.info('main logger instantiated')
+
+    return logger
+
+
+def _get_logfile_name(basepath, hostname):
+    '''format log file as "hostname.log"'''
+    return os.path.join(basepath, '{hostname}.log'.format(hostname=hostname))
+
+
+def get_basepath():
+    '''
+    this method of getting the script's basepath is needed when
+    running the script as a systemd service on the Pi
+    '''
+    return os.path.dirname(os.path.realpath(__file__))
+
+
+def get_hostname():
+    '''get hostname to use for log filename'''
+    return gethostname().split('.')[0]
+
+
+def configure_logger(basepath, hostname):
+    '''read log.yaml config file to configure logging'''
+
+    with open(os.path.join(basepath, 'log.yaml'), 'r') as log_conf:
+        log_config = yaml.safe_load(log_conf)
+
+    log_config['handlers']['file']['filename'] = _get_logfile_name(basepath, hostname)
+    logging.config.dictConfig(log_config)
+    logging.info('* * * * * * * * * * * * * * * * * * * *')
+    logging.info('logging configured')
+
+    return _initialize_logger()
--- main.py
+++ main.py
@@ -1,57 +1,19 @@
 #!/usr/bin/python3
 # send RFID id's over OSC
 # 3/9/18
-# updated 3/16/18
+# updated 3/20/18
 
-import os
 import spi
-import yaml
-import logging
-import logging.config
+import log_util
 from RFIDer import RFIDer
-from socket import gethostname
 from osc_basics import osc_client
-
-# specify the script's full path. this is needed when running script as a service on the Pi
-basepath = '/home/pi/gitbucket/MFRC522-python'
-
-
-def _initialize_logger():
-    logger = logging.getLogger('main')
-    logger.info('main logger instantiated')
-
-    return logger
-
-
-def _get_logfile_name(hostname):
-    '''format log file as "hostname.log"'''
-    return os.path.join(basepath, '{hostname}.log'.format(hostname=hostname))
-
-
-def get_hostname():
-    '''get hostname to use for log filename'''
-    return gethostname().split('.')[0]
-
-
-def configure_logger(hostname):
-    '''read log.yaml config file to configure logging'''
-
-    with open(os.path.join(basepath, 'log.yaml'), 'r') as log_conf:
-        log_config = yaml.safe_load(log_conf)
-
-    log_config['handlers']['file']['filename'] = _get_logfile_name(hostname)
-    logging.config.dictConfig(log_config)
-    logging.info('* * * * * * * * * * * * * * * * * * * *')
-    logging.info('logging configured')
-
-    return _initialize_logger()
 
 
 if __name__ == '__main__':
-    logger = configure_logger(get_hostname())
+    logger = log_util.configure_logger(log_util.get_basepath(), log_util.get_hostname())
     client = osc_client.OSCClient()
 
-    # defaults to one SPI device. RFID reader SDA <-> GPIO pin 24 on the Pi
+    # defaults to one SPI device: RFID reader SDA <-> GPIO pin 24 on the Pi
     # there is another SPI device available on the Pi: SDA <-> GPIO pin 26
     # set num_devices=2 to use both. this is NOT thoroughly tested
     rfider = RFIDer(num_devices=1)
