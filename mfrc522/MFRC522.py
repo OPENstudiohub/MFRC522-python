319570005c5fed7e87dd3ba536e97b9fe4fc2c48
barlaensdoonn <brandon.aleson@gmail.com>
2018-03-24 18:49:13 -0700

    set antenna gain

--- mfrc522/MFRC522.py
+++ mfrc522/MFRC522.py
@@ -430,4 +430,5 @@ class MFRC522:
 
         self.Write_MFRC522(self.TxAutoReg, 0x40)
         self.Write_MFRC522(self.ModeReg, 0x3D)
+        self.Write_MFRC522(self.RFCfgReg, 0x6F)
         self.AntennaOn()
